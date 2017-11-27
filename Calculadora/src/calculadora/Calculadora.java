ea8e2894fab780401ad24655455a10709e1c5c76
Zoni Agustin <agustinzoni@hotmail.com>
2017-11-27 14:12:50 -0300

    realizamos un commit

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,17 +1,40 @@
 package calculadora;
 
+
 public class Calculadora {
-	public static int suma(int a, int b){
-		return a + b;
+	private int ans;
+	public Calculadora(){
+		ans = 0;
 	}
-	public static int resta(int a, int b){
-		return a - b;
+	public int add(int a, int b){
+		ans = a + b;
+		return ans;
 	}
-	public static void main(String args[]){
-		int a = 3, b = 2;
-		int res = suma(a, b);
-		System.out.println("3 + 2 es igual a: "+ res);
-					
+	public int sub(int a, int b){
+		ans = a - b;
+		return ans;
 	}
+	public int add(int v){
+		ans +=v;
+		return ans;
+	}
+	public int sub(int v){
+		ans =+v;
+		return ans;		
+	}
+	public int ans(){
+		return ans;
+	}
+	public void clear(){
+		ans = 0;
+	}
+	public int div(int a, int b){
+		if (b==0){
+			throw new ArithmeticException("No podes dividir por cer!");
+		}
+		ans = a / b;
+		return ans; 
+	}
+	
 	
 }
