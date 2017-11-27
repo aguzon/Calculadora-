4da786849bbabea5aea08a638f67566306e25b52
Zoni Agustin <agustinzoni@hotmail.com>
2017-11-27 11:53:38 -0300

    Se realizo un commint con exito (verde de las operaciones elementales de la calculadora)

--- Calculadora/src/calculadora/CalculadoraTest.java
+++ Calculadora/src/calculadora/CalculadoraTest.java
@@ -1,22 +1,47 @@
 package calculadora;
 
 import static org.junit.Assert.*;
+import org.junit.*;
 
 import org.junit.Test;
 
 public class CalculadoraTest {
-
-	@Test
-	public void testSuma() {
-		int resultado = Calculadora.suma(2, 3);
-		int esperado = 8; //2+3=5
-		assertEquals(esperado, resultado);
-	}
-	@Test
-	public void testResta(){
-		int resultado = Calculadora.resta(3, 2);
-		int esperado = 1; //3-2=1
-		assertEquals(esperado, resultado);
-	}
-
+		Calculadora calc;
+		
+		@Before 
+		public void before(){
+			System.out.println("before()");
+			calc = new Calculadora();
+		}
+		@After
+		public void after(){
+			System.out.println("chau");
+			calc.clear();
+		}
+		@Test
+		public void testSuma() {
+			Calculadora calc = new Calculadora();
+			int resultado = calc.add(3, 2);
+			int esperado = 5; //2+3=5
+			assertEquals(esperado, resultado);
+			System.out.println("se ejecuto el test de la suma");
+		}
+		
+		@Test
+		public void testAnsSum(){
+			Calculadora calc = new Calculadora();
+			calc.add(3, 2);
+			int resultado =  calc.ans();
+			int esperado = 5; //3+2=5
+			assertEquals(esperado, resultado);
+			System.out.println("se ejecuto el test de la resta");
+		}
+		@Test 
+		public void testDividir(){
+			calc.div(5, 2);
+		}
+		@Test(expected = ArithmeticException.class) 
+		public void TestDivCero(){
+			calc.div(5, 0);
+		}
 }
